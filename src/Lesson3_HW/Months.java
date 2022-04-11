7d8e17c8ccb3ed524f084586cdbda22734edb5c6
”Ivam” <ivam@tut.by>
2022-04-11 15:31:18 +0300

    Version 2
    with switch
    
    Signed-off-by: ”Ivam” <ivam@tut.by>

--- src/Lesson3_HW/Months.java
+++ src/Lesson3_HW/Months.java
@@ -10,32 +10,47 @@ package Lesson3_HW;
 
 public class Months {
     public static void main(String[] args) {
-        int month = 5;
-        if (month == 1) System.out.println("Январь");
-        else if (month == 2) {
-            System.out.println("Февраль");
-        } else if (month == 2) {
-            System.out.println("Февраль");
-        } else if (month == 3) {
-            System.out.println("Март");
-        } else if (month == 4) {
-            System.out.println("Апрель");
-        } else if (month == 5) {
-            System.out.println("Май");
-        } else if (month == 6) {
-            System.out.println("Июнь");
-        } else if (month == 7) {
-            System.out.println("Июль");
-        } else if (month == 8) {
-            System.out.println("Август");
-        } else if (month == 9) {
-            System.out.println("Сентябрь");
-        } else if (month == 10) {
-            System.out.println("Октябрь");
-        } else if (month == 11) {
-            System.out.println("Ноябрь");
-        } else if (month == 12) {
-            System.out.println("Декабрь");
+        int month = 3;
+        switch (month) {
+            case 1:
+                System.out.println("Январь");
+                break;
+            case 2:
+                System.out.println("Февраль");
+                break;
+            case 3:
+                System.out.println("Март");
+                break;
+            case 4:
+                System.out.println("Апрель");
+                break;
+            case 5:
+                System.out.println("Май");
+                break;
+            case 6:
+                System.out.println("Июнь");
+                break;
+            case 7:
+                System.out.println("Июль");
+                break;
+            case 8:
+                System.out.println("Август");
+                break;
+            case 9:
+                System.out.println("Сентябрь");
+                break;
+            case 10:
+                System.out.println("Октябрь");
+                break;
+            case 11:
+                System.out.println("Ноябрь");
+                break;
+            case 12:
+                System.out.println("Декабрь");
+                break;
+            default:
+                System.out.println("Вы ввели неправильное число: нет соответствия месяцу");
+
         }
 
     }
