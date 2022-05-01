7201de2b17f7aa7ca58f2682ae87a2991c1e4592
ivam <ivam@tut.by>
2022-05-01 20:20:51 +0300

    Arrays Patterns

--- src/Level2/MyTry/MyArraysPatterns.java
+++ src/Level2/MyTry/MyArraysPatterns.java
@@ -1,11 +1,11 @@
 package Level2.MyTry;
 
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.Random;
 
 public class MyArraysPatterns {
-    public static int [] createRandomArray (int length) {
-        int[] numbers = new int[length];
+    public static int [] createRandomArray (int arrLength) {
+        int[] numbers = new int[arrLength];
         Random numGen = new Random();
         for (int i = 0; i < numbers.length; i++) {
             numbers [i] = numGen.nextInt();
@@ -13,8 +13,8 @@ public class MyArraysPatterns {
 
         return numbers;
     }
-    public static int [] createRandomArray (int length, int bound) {
-        int[] numbers = new int[length];
+    public static int [] createRandomArray (int arrLength, int bound) {
+        int[] numbers = new int[arrLength];
         Random numGen = new Random();
         for (int i = 0; i < numbers.length; i++) {
             numbers [i] = numGen.nextInt(bound);
@@ -22,8 +22,24 @@ public class MyArraysPatterns {
 
         return numbers;
     }
-    public static int [] createRandomArray (int length, int bound, int shiftBound) {
-        int[] numbers = new int[length];
+    public static int [] createRandomArray (int arrLength, int bound, int shiftBound) {
+        int[] numbers = new int[arrLength];
+        Random numGen = new Random();
+        for (int i = 0; i < numbers.length; i++) {
+            numbers [i] = shiftBound + numGen.nextInt(bound-shiftBound+1);
+        }
+
+        return numbers;
+    }
+    public static int [] createSequenceArray (int arrLength) {
+        int[] numbers = new int[arrLength];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers [i] = i;
+        }
+        return numbers;
+    }
+    public static int [] createSequenceArray (int arrLength, int bound, int shiftBound) {
+        int[] numbers = new int[arrLength];
         Random numGen = new Random();
         for (int i = 0; i < numbers.length; i++) {
             numbers [i] = shiftBound + numGen.nextInt(bound-shiftBound+1);
