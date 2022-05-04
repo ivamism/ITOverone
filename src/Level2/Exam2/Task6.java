0c3d33a843ec65bbbed68dc5b1cc400d8c263194
ivam <ivam@tut.by>
2022-05-04 22:19:48 +0300

    Exam 2 Task6

--- src/Level2/Exam2/Task6.java
+++ src/Level2/Exam2/Task6.java
@@ -4,6 +4,29 @@ package Level2.Exam2;
 
 public class Task6 {
     public static void main(String[] args) {
+        int number1 = 64;
+        int number2 = 28;
+        System.out.println("Наибольший общий делитель чисел " + number1 + " и " + number2 + " равен " + findMaxDivisor (number1, number2));
+    }
 
+    private static int findMaxDivisor(int number1, int number2) {
+        int maxDivisor;
+        int tmpbox;
+
+        int min = Math.min(number1, number2);
+        int max = Math.max(number1, number2);
+        while (true) {
+            if (max % min != 0) {
+                tmpbox = min;
+                min = max % min;
+                max = tmpbox;
+            }
+            else {
+                maxDivisor = min;
+                break;
+            }
+        }
+
+        return maxDivisor;
     }
 }
