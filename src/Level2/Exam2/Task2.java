b02d1b1f0e60298e99617edafc001c2ba133b3ff
ivam <ivam@tut.by>
2022-05-09 19:23:53 +0300

    Exam 2 correction

--- src/Level2/Exam2/Task2.java
+++ src/Level2/Exam2/Task2.java
@@ -8,11 +8,11 @@ public class Task2 {
         System.out.println("Среднее арифметическое чисел от 1 до 9 равно - " + findAverage());
     }
 
-    private static int findAverage() {
+    private static double findAverage() {
         int sum = 0;
         for (int i = 1; i <= 9; i++) {
             sum += i;
         }
-        return sum/9;
+        return 1.0 * sum/9;
     }
 }
--- src/Level2/Exam2/Task3.java
+++ src/Level2/Exam2/Task3.java
@@ -4,10 +4,10 @@ package Level2.Exam2;
 
 public class Task3 {
     public static void main(String[] args) {
-        System.out.println("Разница между числами 25 и 7 равна " + findDifferense(25,7));
+        System.out.println("Разница между числами 25 и 7 равна " + findDifference(25,7));
     }
 
-    private static int findDifferense(int a, int b) {
+    private static int findDifference(int a, int b) {
         int min = Math.min(a, b);
         int max = Math.max(a, b);
         return max-min;
